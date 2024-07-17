a28092c741c9e51aae7b55787bc7c8ccf2bb310a
Nishanth Bhat <nishanth.bhat@bfhl.in>
2024-07-17 23:14:49 +0530

    reverting baseurl to localhost

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASEURL = "https://replicache-backend.vercel.app";
-// export const BASEURL = "http://127.0.0.1:8080";
+// export const BASEURL = "https://replicache-backend.vercel.app";
+export const BASEURL = "http://127.0.0.1:8080";
 
 const api = axios.create({
   baseURL: BASEURL,
